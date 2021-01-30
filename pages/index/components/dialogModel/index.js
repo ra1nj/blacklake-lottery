c8d643456b5fbc83745a830c472b79131b5e2e8e
rain <jiangran@blacklake.cn>
2021-01-30 20:44:23 +0800

    页面框架

--- pages/index/components/dialogModel/index.js
+++ pages/index/components/dialogModel/index.js
@@ -5,31 +5,34 @@ Component({
   properties: {
     isShow: {
       type: Boolean,
-      value: true
+      value: true,
+      observer: function (newVal, oldVal, changedPath) {
+        // 通常 newVal 就是新设置的数据， oldVal 是旧数据
+        console.log('isshow', newVal)
+        if (newVal == true) {
+          this.starAnimation()
+        }else{
+          this.setData({
+            left:0,
+          })
+        }
+      }
     },
-    dialogId: {
-      type: Number,
-      value: 1
-    },
-    codeList:{
-      type: Array,
-      value:[1,2,3,4,5]
-    },
-    fieldNumber: {
-      type: Number,
-      value: 2
-    },
-    typeTips: {
-      type: String,
-      value: '将于3月8日20点来开奖哦！'
+    currentItem: {
+      type: Object,
+      value: null,
     }
   },
 
   /**
    * 组件的初始数据
    */
-  data: {},
-
+  data: {
+    left: 0,
+    round:new Array(10).fill('1'), //转的圈数
+    duration: 16, //转的时间 需要可以被开方
+    tip:'开奖中...',
+  },
   /**
    * 组件的方法列表
    */
@@ -40,5 +43,41 @@ Component({
     closeModel() {
       this.triggerEvent('closeModel')
     },
+    starAnimation() {
+      console.log('Animation start')
+      let candidateList = this.properties.currentItem.candidateList;
+      let winnerList = this.properties.currentItem.winnerList;
+      let winnerIndex
+      for(let index in candidateList){
+        let c = candidateList[index]
+        if(winnerList[0].id == c.id){
+          winnerIndex = index
+        }
+      }
+      let totalStep = Number(((this.data.round.length -1) * candidateList.length))+Number(winnerIndex)
+      console.log('winnder index',winnerIndex,' total step',totalStep)
+      let singleAvatarLength = 140;
+      let totalLength = singleAvatarLength * totalStep;
+      let singleMoveLength = totalLength / this.data.duration
+      let speed = 0;
+      let isBrake = false;
+      let left = this.data.left;
+
+      let timer = setInterval(() => {
+        if (speed == Math.sqrt(this.data.duration)) isBrake = true;
+        speed = speed + (isBrake ? -1 : +1);
+        if (speed == 0 && timer != null) {
+          clearInterval(timer)
+          this.setData({
+            tip: `恭喜 ${winnerList[0].nickName} ${winnerList[1].nickName} 俩位同学中奖！`
+          })
+        }
+        left -= speed * singleMoveLength
+        console.log('moving,current speed', speed)
+        this.setData({
+          left: left
+        })
+      }, 1000)
+    }
   }
 });
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,170 +1,359 @@
 const app = getApp()
 
+function groupBy(list, name) {
+  return list.reduce((obj, item) => {
+    if (!obj[item[name]]) {
+      obj[item[name]] = []
+      obj[item[name]].push(item)
+    } else {
+      obj[item[name]].push(item)
+    }
+    return obj
+  }, {})
+}
+
+function loading(){
+  tt.showLoading({
+    title: '请求中...请稍后',
+    mask:true,
+  });
+}
+function hideLoading(){
+  tt.hideLoading({
+    success (res) {
+      console.log(`res`);
+    },
+    fail (res) {
+      console.log(`hideLoading failure`);
+    }
+  });
+}
+
 Page({
   data: {
-    userInfo:undefined,
-    remainTicketAmount:11,
-    prizes:[
-      {
-        categoryName:'特等奖',
-        prizeList:[
-          {
-            name:'Apple iPhone 12 Pro Max',
-            imgurl:'https://public-template.s3.cn-northwest-1.amazonaws.com.cn/抽奖奖品/一等奖PS5.jpg',
-            prizeStatus:0,
-            totalBetNum:122,
-            candidateList:[
-              {
-                id:1,
-                nickName:'rain',
-                avatarUrl:'xxx',
-              },
-            ],
-            winnerList:[
-              {
-                id:1,
-                nickName:'rain',
-                avatarUrl:'xxx',
-              },
-            ],
-    
-          }
-        ],
-      }
-    ],
+    userInfo: undefined,
+    remainTicketAmount: 11,
+    prizes: [],
+    currentItem: {},
+    openingPrize: false,
+    showDetail: false,
+    userBetInfo:{},
     betList:{},
-    openingPrize:false,
-    showDetail:false,
-    currentItem:{},
-    isAnimate:false,
-    left:-7000,
-    candidateList:new Array(100),
   },
-  showRule(){
+  showRule() {
     tt.showModal({
       title: '抽奖规则',
-      showCancel:false,
+      showCancel: false,
       content: '每个账号只有11张投票券，可以任意投给奖池里面的38种奖品（至少1张，最多11张），可以实时自行修改投票，每种奖品2人获奖。2月2日24：00停止投票',
     })
   },
-  startAnimation(){
-
-    let singleAvatarLength = 70;
-    let totalLength = singleAvatarLength * 100 ;
-    let singleMoveLength = totalLength / 9
-    let speed = 0;
-    let isBrake = false;
-    let left = this.data.left;
-  
-    let timer = setInterval(()=>{
-      if(speed == 3) isBrake = true;
-      speed = speed + (isBrake? -1: +1);
-      if(speed == 0 && timer != null){
-        clearInterval(timer)
-      }
-      left += speed * singleMoveLength
-      console.log('moving,current speed',speed)
-      this.setData({
-        left: left
-      })
-    },1000)
-    // this.setData({
-    //   isAnimate:!this.data.isAnimate,
-    // })
-  },
   handleModel() {
     this.setData({
-      showDetail: !this.data.showDetail
+      showDetail: false
     })
   },
-  closeDetail:function(event){
-    this.setData({
-      showDetail:false,
-    })
-  },
-  checkDetail:function(event){
-    let item = event.target.dataset['item']
-
-    this.setData({
-      currentItem:item,
-      showDetail:true,
-    })
-    this.animate()
-  },
-  bet:function(event){
+  bet: function (event) {
+    if (this.data.remainTicketAmount <= 0) {
+      tt.showToast({
+        title: '您没有额外的金币了，不能再加啦！',
+        icon: 'warning'
+      })
+      return
+    }
     let betList = this.data.betList;
     let betId = event.target.dataset['bet'].id
-    if(betList[betId]){
+    if (betList[betId]) {
       betList[betId] += 1
-    }else{
+    } else {
       betList[betId] = 1
     }
     this.setData({
       remainTicketAmount: this.data.remainTicketAmount - 1,
-      betList:betList,
+      betList: betList,
     })
+    const putCoinSound = tt.createInnerAudioContext();
+    putCoinSound.src = '../../assets/sound/putcoin.mp3';
+    putCoinSound.play()
   },
-  clearBet:function(){
+  clearBet: function () {
+    var that = this
+    loading()
+    tt.request({
+      url: `${app.globalData.baseUrl}/lottery/reset`,
+      method:'POST',
+      data: {
+        openId:tt.getStorageSync('open_id')
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        console.log(`重置 调用成功 res`);
+        that.getUserBetInfo()
+        hideLoading()
+      },
+      fail(res) {
+        console.log(`重置 调用失败`);
+        hideLoading()
+      }
+    })
     this.setData({
-      betList:{},
-      remainTicketAmount:11,
+      betList: {},
     })
-    this.startAnimation();
   },
-  getUserInfo: function (){
-    var that = this;
-    tt.getUserInfo({
-      success (res) {
-        console.log(res)
+  submit(){
+    let voteList = []
+    for(let key in this.data.betList){
+      let value = this.data.betList[key];
+      voteList.push({
+        rewardId:key,
+        voteCount:value,
+      })
+    }
+    var that = this
+    loading()
+    tt.request({
+      url: `${app.globalData.baseUrl}/lottery/vote`,
+      method:'POST',
+      data: {
+        openId:tt.getStorageSync('open_id'),
+        voteList:voteList,
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        console.log(`提交成功`);
+        that.getUserBetInfo()
+        hideLoading()
+      },
+      fail(res) {
+        console.log(`request 调用失败`);
+        hideLoading()
+      }
+    })
+  },
+  openPrize(event){
+    let prize = event.target.dataset['prize']
+    loading()
+    tt.request({
+      url: `${app.globalData.baseUrl}/lottery/lottery`,
+      method:'POST',
+      data: {
+        id:prize.id,
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        console.log(`开奖 调用成功 ${res}`);
+        hideLoading()
+      },
+      fail(res) {
+        console.log(`开奖 调用失败`);
+        hideLoading()
+      }
+    })
+    this.setData({
+      currentItem:prize,
+      showDetail:true,
+    })
+  },
+  getPrizeList() {
+    var that = this
+    loading()
+    let task = tt.request({
+      url: `${app.globalData.baseUrl}/lottery/getRewardList`,
+      method: 'POST',
+      data: {},
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        // console.log(`request 调用成功 ${JSON.stringify(res)}`);
+        let group = groupBy(res.data.data,'level')
+        let covertData = []
+        for(let index in group){
+          let i = group[index]
+          let categoryName = ['特等奖','一等奖','二等奖','三等奖'][i[0].level]
+          covertData.push({
+            categoryName:categoryName,
+            prizeList:i,
+          })
+        }
         that.setData({
-          userInfo:res.userInfo
-        },()=>{
+          prizes: covertData
+        })
+        hideLoading();
+      },
+      fail(res) {
+        console.log(`request 调用失败${JSON.stringify(res)}`);
+        hideLoading()
+      }
+    });
+  },
+  getUserBetInfo(){
+    let openId = tt.getStorageSync('open_id')
+    var that = this
+    if(!!openId){
+      tt.request({
+        url: `${app.globalData.baseUrl}/lottery/getUserDetail`,
+        method:'POST',
+        data: {
+          openId: openId
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success(res) {
+          console.log(`用户下注信息 调用成功 ${JSON.stringify(res)}`);
+          that.setData({
+            userBetInfo:res.data.data,
+            remainTicketAmount: res.data.data.allTickets - res.data.data.usedTickets
+          })
+        },
+        fail(res) {
+          console.log(`request 调用失败`);
+          tt.showModal({
+            title: '',
+            content: `${JSON.stringify(res)}`,
+          })
+        }
+      })
+    }else{
+      this.initUser()
+    }
+  },
+  getUserInfo: function () {
+    var that = this;
+    let localUserInfo = tt.getStorageSync('userInfo')
+    console.log('local userinfo', localUserInfo)
+    if (!!localUserInfo.nickName) {
+      this.setData({
+        userInfo: localUserInfo
+      })
+    } else {
+      tt.getUserInfo({
+        success(res) {
+          console.log(res)
+          tt.setStorageSync('userInfo', res.userInfo)
+          that.setData({
+            userInfo: res.userInfo
+          }, () => {
+            that.showWelcome()
+          })
+        },
+        fail(res) {
+          console.log('fail to get userinfo')
+        }
+      })
+    }
+  },
+  initUser(code) {
+    var that = this
+    tt.getUserInfo({
+      success(ures) {
+        console.log('获取用户信息成功', ures)
+        tt.setStorageSync('userInfo', ures.userInfo)
+        that.setData({
+          userInfo: ures.userInfo
+        }, () => {
           that.showWelcome()
         })
+        tt.request({
+          url: 'https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal/',
+          data: {
+            app_id: 'cli_a09b9ba2a47dd00e',
+            app_secret: '5Axk9L277KmJz1ZfXirItbteI3lk1Vt6'
+          },
+          header: {
+            'content-type': 'application/json'
+          },
+          success(res) {
+            console.log(`请求access token 调用成功 ${JSON.stringify(res)}`);
+            tt.request({
+              url: 'https://open.feishu.cn/open-apis/mina/v2/tokenLoginValidate',
+              data: {
+                code: code,
+                token: res.data.app_access_token,
+              },
+              header: {
+                'content-type': 'application/json'
+              },
+              success(res) {
+                console.log(`获取openid 调用成功 ${JSON.stringify(res)}`);
+                tt.setStorageSync('open_id', res.data.data.open_id)
+                let userInfo = ures.userInfo
+                tt.request({
+                  url: `${app.globalData.baseUrl}/lottery/initUser`,
+                  method:'POST',
+                  data: {
+                    name: userInfo.nickName,
+                    headPictureUrl: userInfo.avatarUrl,
+                    openId: res.data.data.open_id
+                  },
+                  header: {
+                    'content-type': 'application/json'
+                  },
+                  success(res) {
+                    console.log(`初始化用户 调用成功`);
+                    that.getUserBetInfo()
+                  },
+                  fail(res) {
+                    console.log(`request 调用失败`);
+                  }
+                })
+
+              },
+              fail(res) {
+                console.log(`request 调用失败`);
+              }
+            })
+          },
+          fail(res) {
+            console.log(`request 调用失败`);
+          }
+        })
       },
-      fail(res){
+      fail(res) {
         console.log('fail to get userinfo')
       }
     })
+
   },
   onLoad: function () {
-    let userInfo = tt.getStorageSync('userInfo')
-    this.setData({
-      userInfo:userInfo,
-    })
-    if(this.data.userInfo.nickName){
-      this.showWelcome()
-    }else{
-      this.getUserInfo();
-    }
-    // this.getPrizeList()
-  },
-  getPrizeList(){
     var that = this
-    let task = tt.request({
-      url: 'http://172.168.2.69:8099/lottery/getRewardList',
-      method:'POST',
-      data:{},
-      header: {
-          'content-type': 'application/json'
+    tt.checkSession({
+      success(res) {
+        console.log(`session 未过期`);
+        that.getUserInfo()
       },
-      success (res) {
-          console.log(`request 调用成功 ${JSON.stringify(res)}`);
-          that.setData({
-            prizeList:res.data.data,
-          })
-      },
-      fail (res) {
-          console.log(`request 调用失败${JSON.stringify(res)}`);
+      fail(res) {
+        console.log(`session 已过期，需要重新登录`);
+        that.login()
       }
-  });
+    })
+    this.getPrizeList()
+    this.getUserBetInfo()
 
+  },
+  login() {
+    var that = this
+    tt.login({
+      success(res) {
+        console.log('login success')
+        that.initUser(res.code)
+      },
+      fail() {
+        console.log('login fail')
+      }
+    })
   },
   showWelcome: function () {
     tt.showToast({
-      title:`Welcome to Balcklake Casino,${this.data.userInfo.nickName}`,
-      icon:'none',
-      duration:2000,
+      title: `Welcome to Balcklake Casino,${this.data.userInfo.nickName},good luck!`,
+      icon: 'none',
+      duration: 2000,
     })
   }
 })
